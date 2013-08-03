6fad74b951a96f7b47d30a70199f89d356c7d329
Jimmy <gosu.snp@gmail.com>
2013-08-03 02:03:15 +0200

    Improve parsing of ngOptions.
    * Use regex from the implementation of the select directive in ng.

--- src/options-proxy.js
+++ src/options-proxy.js
@@ -5,21 +5,31 @@
  */
 var ngOptionsParser = function(ngOptionString) {
     var attributeName,
-        optionList,
+        values,
         newNgOptionString;
-    var labelForValueInArray = /^((\w+\.)+(\w+))\s+for\s.*\sin\s(\w+)$/,
-        match;
+    var match,
+        submatch;
+    // NG_OPTIONS_REGEXP from the select directive implementation of angular.
+    var NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w\d]*)|(?:\(\s*([\$\w][\$\w\d]*)\s*,\s*([\$\w][\$\w\d]*)\s*\)))\s+in\s+(.*?)(?:\s+track\s+by\s+(.*?))?$/,
+        PIPE_REGEXP = /^\s*([^\s]+)\s*\|/,
+        AS_REGEXP = /^\s*(.*)\s+as\s+(.*)$/;
 
-    match = labelForValueInArray.exec(ngOptionString);
-    if (match) {
-        attributeName = match[3];
-        optionList = match[4];
-        newNgOptionString = match[1]+' as '+ngOptionString;
+    if (match = NG_OPTIONS_REGEXP.exec(ngOptionString)) {
+        var valueName = match[4] || match[6],
+            displayName = match[2] || match[1];
+        attributeName = displayName.substring(valueName.length+1, displayName.length);
+        values = match[7];
+        if (submatch = PIPE_REGEXP.exec(values))
+            values = submatch[1];
+        if (submatch = AS_REGEXP.exec(ngOptionString))
+            newNgOptionString = displayName+' as '+submatch[2];
+        else
+            newNgOptionString = displayName+' as '+ngOptionString;
     }
 
     return {
         attributeName: attributeName,
-        optionList: optionList,
+        values: values,
         ngOptionString: newNgOptionString,
     };
 }
@@ -35,12 +45,12 @@ angular.module('options-proxy', []).directive('optionsProxy', function() {
                 modelProxyVarName = modelVarName + 'Proxy',
                 ngOptionString = tAttrs.ngOptions,
                 attributeName,
-                optionList;
+                values;
 
             // Parse ngOptions
             var parsedOptions = ngOptionsParser(ngOptionString);
             attributeName = parsedOptions.attributeName;
-            optionList = parsedOptions.optionList;
+            values = parsedOptions.values;
             tAttrs.$set('ngOptions', parsedOptions.ngOptionString);
             tAttrs.$set('ngModel', modelProxyVarName);
 
@@ -50,7 +60,7 @@ angular.module('options-proxy', []).directive('optionsProxy', function() {
                     scope.modelVarName = modelVarName;
                     scope.modelProxyVarName = modelProxyVarName;
                     scope.attributeName = attributeName;
-                    scope.optionList = optionList;
+                    scope.values = values;
 
                     // Data binding for proxy
                     // Bind on modelVarName to track direct modifications of variable
@@ -60,9 +70,9 @@ angular.module('options-proxy', []).directive('optionsProxy', function() {
                     });
                     // Bind on modelProxyVarName to forward of the proxy variable
                     scope.$watch(scope.modelProxyVarName, function(newValue, oldValue) {
-                        var optionList = scope[scope.optionList];
-                        for (var i in optionList) {
-                            var option = optionList[i];
+                        var values = scope[scope.values];
+                        for (var i in values) {
+                            var option = values[i];
                             if (newValue == option[scope.attributeName]) {
                                 if (scope[scope.modelVarName][scope.attributeName] != option[scope.attributeName]) {
                                     scope[scope.modelVarName] = angular.copy(option);
--- test/ngOptionsParserTest.js
+++ test/ngOptionsParserTest.js
@@ -2,8 +2,40 @@ describe("ngOptionsParser", function() {
     it("should test basic patterns for ngOptionsParser", function() {
         expect(ngOptionsParser("obj.name for obj in objects")).toEqual({
             attributeName: 'name',
-            optionList: 'objects',
+            values: 'objects',
             ngOptionString: 'obj.name as obj.name for obj in objects',
+        });
+    });
+
+    it("is a pattern test with filter", function() {
+        expect(ngOptionsParser("obj.name for obj in objects | filter: {type: 'object'}")).toEqual({
+            attributeName: 'name',
+            values: 'objects',
+            ngOptionString: 'obj.name as obj.name for obj in objects | filter: {type: \'object\'}',
+        });
+    });
+
+    it("is a pattern test (2)", function() {
+        expect(ngOptionsParser("obj.name for obj in objects| filter: {type: 'object'}")).toEqual({
+            attributeName: 'name',
+            values: 'objects',
+            ngOptionString: 'obj.name as obj.name for obj in objects| filter: {type: \'object\'}',
+        });
+    });
+
+    it("is an pattern test (3)", function() {
+        expect(ngOptionsParser("t as t.name for t in types | filter: {type: 'object'}")).toEqual({
+            attributeName: 'name',
+            values: 'types',
+            ngOptionString: "t.name as t.name for t in types | filter: {type: 'object'}",
+        });
+    });
+
+    it("is an pattern test (4)", function() {
+        expect(ngOptionsParser("t as t.name group by (t.subtype + ' ' + t.type) for t in types")).toEqual({
+            attributeName: 'name',
+            values: 'types',
+            ngOptionString: "t.name as t.name group by (t.subtype + ' ' + t.type) for t in types",
         });
     });
 });
